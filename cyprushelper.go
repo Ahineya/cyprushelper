cc2af626452edb83c55ae0b78eb2f25225d574bc
Pavel Evsegneev <deadswallow@gmail.com>
2016-05-18 00:24:35 +0300

    Add cities to pollution. Show full pollution data

--- cyprushelper.go
+++ cyprushelper.go
@@ -195,13 +195,21 @@ func sendSeaTemp(bot *tgbotapi.BotAPI, update tgbotapi.Update, tokens []string) 
 }
 
 func sendPollution(bot *tgbotapi.BotAPI, update tgbotapi.Update, tokens []string) {
-	pollutionResult, err := pollution.GetPollution("")
+	if len(tokens) < 2 {
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Please, define city: /pollution city_name")
+		bot.Send(msg)
+		return
+	}
+
+	city := strings.ToLower(tokens[1])
+	pollutionResult, err := pollution.GetPollution(city)
 	if err != nil {
 		msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Can't find pollution info for " + strings.Join(tokens[1:], " ") + ", error happen: " + err.Error())
 		bot.Send(msg)
+		return;
 	}
 
-	msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Pollution in " + strings.Join(tokens[1:], " ") + "is: " + pollution.FormatPollution(pollutionResult))
+	msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Pollution in " + strings.Join(tokens[1:], " ") + " is:\n\n" + pollution.FormatPollution(pollutionResult))
 	msg.ParseMode = tgbotapi.ModeHTML
 	bot.Send(msg)
 }
--- pollution/pollution.go
+++ pollution/pollution.go
@@ -8,6 +8,8 @@ import (
 	"time"
 	"strings"
 	"errors"
+	"strconv"
+	"fmt"
 )
 
 const pollution_url = "http://www.airquality.dli.mlsi.gov.cy/site/ajax_exec"
@@ -27,12 +29,24 @@ type PollutionResult struct {
 	DateTo   string `json:"date_to"`
 }
 
-// TODO: Refactor. Add cities.
-func GetPollution(city string) (*PollutionData, error) {
+// TODO: Refactor.
+func GetPollution(city string) (*PollutionResult, error) {
+	// TODO: this API updates every hour, but not in :00 minutes. There is a gap to :15 minutes now. Need to fix.
 	from := time.Now().Add(-time.Hour).Format("02/01/2006+15:04")
 	to := time.Now().Format("02/01/2006+15:04")
 
-	request, err := url.Parse(`ajax_call=1&post_data[action]=get_plot_values_sd_dates&post_data[station_code]=LIMTRA&post_data[date_from]=`+ from +`&post_data[date_to]=`+ to +`&post_data[db_period]=1-hour`)
+	city = normalizeCity(city)
+	if city == "" {
+		return nil, errors.New("There is no info for this city")
+	}
+
+	requestStr := `ajax_call=1&post_data[action]=get_plot_values_sd_dates`
+	requestStr += `&post_data[station_code]=` + city
+	requestStr += `&post_data[date_from]=`+ from
+	requestStr += `&post_data[date_to]=`+ to
+	requestStr += `&post_data[db_period]=1-hour`
+
+	request, err := url.Parse(requestStr)
 	if err != nil {
 		return nil, err
 	}
@@ -63,20 +77,78 @@ func GetPollution(city string) (*PollutionData, error) {
 	}
 
 	if len(pollutionResult.Data) == 0 {
-		return nil, errors.New("There is no pollution data for this time and city")
+		return nil, errors.New(`There is no pollution data for this time, you can repeat the request later.
+					Usually this happens at the beginning of an hour.`)
 	}
 
-	for _, data := range pollutionResult.Data {
-		if data.PollutantCode == "PM10" {
-			return &data, nil
-		}
-	}
-
-	return nil, errors.New("There is no pollution data for this time and city")
+	return &pollutionResult, nil
 }
 
-func FormatPollution(pollution *PollutionData) string {
+func FormatPollution(pollution *PollutionResult) string {
 	result := ""
-	result += "<b>" + pollution.PollutantCode + "</b>: " + pollution.Value
+
+	for _, pollutionData := range pollution.Data {
+		result += getPollutionLevel(pollutionData.PollutantCode, pollutionData.Value) + " "
+		result += normalizePollutantCode(pollutionData.PollutantCode) + ": "
+		result += "<b>" + pollutionData.Value + "</b> μg/m³\n"
+	}
+
 	return result
+}
+
+func normalizeCity(city string) string {
+	if city == "limassol" || city == "lemesos" {
+		return "LIMTRA"
+	}
+
+	if city == "pafos" || city == "paphos" {
+		return "PAFTRA"
+	}
+
+	if city == "larnaka" || city == "larnaca" {
+		return "LARTRA"
+	}
+
+	if city == "lefcosia" || city == "nicosia" {
+		return "NICTRA"
+	}
+
+	return ""
+}
+
+
+
+func normalizePollutantCode(pollutantCode string) string {
+	pollutants := make(map[string]string)
+	pollutants["NO"] = "Nitrogen Oxide"
+	pollutants["NO2"] = "Nitrogen Dioxide"
+	pollutants["NOx"] = "Nitrogen Oxides"
+	pollutants["SO2"] = "Sulfur Dioxide"
+	pollutants["O3"] = "Ozone"
+	pollutants["CO"] = "Carbon Monoxide"
+	pollutants["PM10"] = "Particulate Matter 10 μm"
+	pollutants["PM25"] = "Particulate Matter 2.5 μm"
+	pollutants["BEN"] = "Benzene"
+
+	return pollutants[pollutantCode]
+}
+
+func getPollutionLevel(pollutantCode string, value string) string {
+	intValue, err := strconv.ParseFloat(value, 64)
+	if err != nil {
+		fmt.Println(err.Error())
+		return ""
+	}
+	if pollutantCode == "PM10" {
+		if intValue < 50 {
+			return "[<b>Low</b>]"
+		} else if intValue >= 50 && intValue < 100 {
+			return "[<b>Moderate</b>]"
+		} else if intValue >= 100 && intValue < 200 {
+			return "[<b>High</b>]"
+		} else if intValue >= 200 {
+			return "[<b>Very high</b>]"
+		}
+	}
+	return ""
 }
